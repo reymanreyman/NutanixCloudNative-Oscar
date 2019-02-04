02274eed53836669e0db9065baf829acbe879b0f
Michael Haigh <michael.haigh@nutanix.com>
2019-02-04 10:14:40 -0800

    Added tagline comment

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -366,6 +366,7 @@ from oscar.defaults import *
 if os.environ.get('S3_ENDPOINT_URL') == 'http://s3.amazonaws.com':
     OSCAR_SHOP_TAGLINE = os.environ.get('SHOP_TAGLINE', 'Running on AWS')
 else:
+    # NCN Tagline
     OSCAR_SHOP_TAGLINE = os.environ.get('SHOP_TAGLINE', 'Running on Nutanix Cloud Native')
 
 # Meta
