2e197e4f8d00653ee0e56ffaabbe31b22d273a70
Michael Haigh <michael.haigh@nutanix.com>
2019-03-07 07:13:19 -0800

    Reverted tagline

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -368,7 +368,7 @@ if os.environ.get('S3_ENDPOINT_URL') == 'http://s3.amazonaws.com':
     OSCAR_SHOP_TAGLINE = os.environ.get('SHOP_TAGLINE', 'Running on AWS')
 else:
     # NCN Tagline
-    OSCAR_SHOP_TAGLINE = os.environ.get('SHOP_TAGLINE', 'Running on Nutanix Cloud Native, Built for Orlando Tech Summit')
+    OSCAR_SHOP_TAGLINE = os.environ.get('SHOP_TAGLINE', 'Running on Nutanix Cloud Native')
 
 # Meta
 # ====
