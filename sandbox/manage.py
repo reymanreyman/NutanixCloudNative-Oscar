9d8b45ed2df669643c352efcd02bcf91c22352e8
Michael Haigh <michael.haigh@nutanix.com>
2019-04-25 10:30:14 -0700

    Added Django function, updated shop tagline

--- sandbox/manage.py
+++ sandbox/manage.py
@@ -6,5 +6,6 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
     from django.core.management import execute_from_command_line
+    from django.core.management import get_comands
 
     execute_from_command_line(sys.argv)
